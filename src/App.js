23f1162a2fc852147bbb0a0d82e98a456d64ab9b
Your Name <you@example.com>
2018-11-20 11:17:25 +0100

    wetherOutput

--- src/App.js
+++ src/App.js
@@ -7,13 +7,40 @@ const API_KEY = "3ddaf3a5ea045a6cbae9286f0f29c6f8";
 
 class App extends React.Component {
 
+  state = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined
+  }
+
 gettingWeather = async (e) => {
   e.preventDefault();
   const city = e.target.elements.city.value;
-  const api_url = await
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
-  const data = await api_url.json();
-  console.log(data);
+
+
+  if(city) {
+
+    const api_url = await
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+    const data = await api_url.json();
+
+    var sunset = data.sys.sunset;
+    var date = new Date();
+    date.setTime(sunset);
+    var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+    this.setState({
+      temp: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      pressure: data.main.pressure,
+      sunset: sunset_date,
+      error: ""
+    });
+  }
 }
 
   render() {
@@ -21,7 +48,14 @@ gettingWeather = async (e) => {
       <div>
         <Info />
         <Form  weatherMethod={this.gettingWeather}/>
-        <Weather />
+        <Weather
+          temp={this.state.temp}
+          city={this.state.city}
+          country={this.state.country}
+          pressure={this.state.pressure}
+          sunset={this.state.sunset}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -3,7 +3,16 @@ import React from "react";
 class Weather extends React.Component {
   render() {
     return(
-      <div>Component with weather info</div>
+      <div>
+        { this.props.city &&
+          <div>
+            <p>Place: {this.props.city}, {this.props.country}</p>
+            <p>Temperature: {this.props.temp}</p>
+            <p>Pressure: {this.props.pressure}</p>
+            <p>Sunset: {this.props.sunset}</p>
+          </div>
+        }
+        </div>
     );
   }
 }
