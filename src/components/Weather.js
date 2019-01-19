c4205966bdac2acadd407e4939d461d2c43c901c
Your Name <you@example.com>
2019-01-19 01:11:19 +0100

    styles

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Weather = props => (
-  <div>
+  <div className="infoWeath">
     {props.city &&
       <div>
         <p>Place: {props.city}, {props.country}</p>
@@ -10,7 +10,7 @@ const Weather = props => (
         <p>Sunset: {props.sunset}</p>
       </div>
     }
-    <p>{props.error}</p>
+    <p className="error">{props.error}</p>
     </div>
 )
 
